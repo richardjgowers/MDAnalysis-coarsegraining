b7a8a0ca66899d1c50a2b2e5c531b70c6487f43f
richardjgowers <richardjgowers@gmail.com>
2019-08-23 13:54:29 +0100

    added octanol testcase

--- test_cguniverse.py
+++ test_cguniverse.py
@@ -1,8 +1,8 @@
 # coarse graining in MDAnalysis
-# Copyright (c) 2015 Richard J Gowers
+# Copyright (c) 2015, 2019 Richard J Gowers
 # Released under the GNU Lesser General Public License, version 2 or later.
 
-from numpy.testing import TestCase, assert_equal, assert_allclose
+from numpy.testing import assert_allclose
 
 import MDAnalysis as mda
 from cguniverse import CGUniverse
@@ -11,21 +11,37 @@ import pytest
 
 
 @pytest.fixture
-def at_universe():
-    return mda.Universe('testdata/new.gro')
+def water_at_universe():
+    return mda.Universe('testdata/water.gro')
 
 @pytest.fixture
-def cg_universe():
+def water_cg_universe():
     mapping = {'SOL': [[0, 1, 2]]}
 
-    return CGUniverse('testdata/new.gro', mapping=mapping)
+    return CGUniverse('testdata/water.gro', mapping=mapping)
 
 
-def test_creation(cg_universe):
-    assert_equal(len(cg_universe.atoms), 3)
+def test_creation(water_cg_universe):
+    assert len(water_cg_universe.atoms) == 3
 
 
-def test_pos1(at_universe, cg_universe):
+def test_pos1(water_at_universe, water_cg_universe):
     """First "atom" position should be COM of first residue"""
-    for res, atom in zip(at_universe.residues, cg_universe.atoms):
+    for res, atom in zip(water_at_universe.residues, water_cg_universe.atoms):
         assert_allclose(res.atoms.center_of_mass(), atom.position)
+
+
+@pytest.fixture
+def oct_atu():
+    return mda.Universe('testdata/octanol.gro')
+
+@pytest.fixture
+def oct_cgu():
+    return CGUniverse('testdata/octanol.gro',
+                      mapping={'OcOH':[list(range(11)), list(range(11, 27))]})
+
+
+def test_residue_com(oct_atu, oct_cgu):
+    # check that the COM of the first residue is unchanged
+    assert_allclose(oct_atu.residues[0].atoms.center_of_mass(),
+                    oct_cgu.residues[0].atoms.center_of_mass())
