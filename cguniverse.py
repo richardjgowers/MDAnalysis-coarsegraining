4854ca658c30e454ef55d65576d7799fdf5b0516
richardjgowers <richardjgowers@gmail.com>
2019-08-23 13:34:17 +0100

    updated for MDAnalysis v0.19
    merged into single file

--- cguniverse.py
+++ cguniverse.py
@@ -1,5 +1,5 @@
 # coarse graining in MDAnalysis
-# Copyright (c) 2015 Richard J Gowers
+# Copyright (c) 2015, 2019 Richard J Gowers
 # Released under the GNU Lesser General Public License, version 2 or later.
 
 """Universe object for coarse graining in MDAnalysis toolkit.
@@ -16,39 +16,80 @@ cgu.atoms.positions # returns the positions of the beads
 
 """
 import MDAnalysis as mda
-from MDAnalysis.core.AtomGroup import Atom, AtomGroup
+from MDAnalysis.coordinates import base
+from MDAnalysis.core import topologyattrs as ta
+from MDAnalysis.core import topology
+import numpy as np
 
-from cgtraj import CGTraj
 
+class CGTraj(base.ProtoReader):
+    """Fakes a coarse grained trajectory object
 
-class Bead(Atom):
-    """Building block for CG Universe
+    Takes an atomistic trajectory and list of beads and manipulates both
+    to recreate a reader for a coarse grained Universe.
 
-    Should act like an Atom, but reports a mass weighted average on the
-    atoms within.
+    Would also probably work as a standalone thingy for writing out
+    coarse grained trajectories.
     """
+    def __init__(self, atomistic_traj, mapping, weights):
+        """
+        Parameters
+        ----------
+        atomistic_traj : mda.Reader
+          atomistic trajectory
+        mapping : numpy array
+          which bead
+        weights : numpy array
+          for each atom, what weight
+        """
+        super(base.ProtoReader, self).__init__()
 
-    __slots__ = (
-        "atoms",
-        "number", "id", "name", "type", "resname", "resid", "segid",
-        "mass", "charge", "residue", "segment",
-        "__universe",
-        "radius", "bfactor", "resnum", "serial", "altLoc")
+        self._t = atomistic_traj
+        self._mapping = mapping
+        self._weights = weights
 
-    def __init__(self, atoms, *args, **kwargs):
-        """atoms should be an AtomGroup representing the bead"""
-        self.atoms = AtomGroup(atoms)
-        super(Bead, self).__init__(*args, ** kwargs)
+        self.n_atoms = mapping.max() + 1
+
+        self.ts = base.Timestep(self.n_atoms)
+        self._fill_ts(self._t.ts)
+
+    def _read_next_timestep(self):
+        """Return the next timestep"""
+        # Get the next TS from the atom trajectory
+        at_ts = self._t.next()
+
+        self._fill_ts(at_ts)
+
+        return self.ts
+
+    def _read_frame(self, frame):
+        """Return a single frame"""
+        at_ts = self._t[frame]
+
+        self._fill_ts(at_ts)
+
+        return self.ts
+
+    def _fill_ts(self, other_ts):
+        """Rip information from atomistic TS into our ts
+
+        Make positions based on COM of our beads.
+        """
+        self.ts.frame = other_ts.frame
+        self.ts._unitcell = other_ts._unitcell
+
+        other_ts.positions *= self._weights[:, None]
+        np.add.at(self.ts._pos, self._mapping, other_ts.positions)
+
+    def _reopen(self):
+        self._t._reopen()
+
+    @property
+    def n_frames(self):
+        return len(self._t)
 
     def __repr__(self):
-        return ("<Bead {idx}: {name} of type {t} of resname {rname}, "
-                "resid {rid} and segid {sid}{altloc}"
-                " representing {natoms} atoms>".format(
-                    idx=self.number + 1, name=self.name, t=self.type,
-                    rname=self.resname, rid=self.resid, sid=self.segid,
-                    altloc="" if not self.altLoc
-                    else " and altloc {}".format(self.altLoc),
-                    natoms=len(self._atoms)))
+        return "<CG Trajectory doing {} beads >".format(len(self.beads))
 
 
 class CGUniverse(mda.Universe):
@@ -69,46 +110,58 @@ class CGUniverse(mda.Universe):
 
         Note that the indices inside the mapping refer to the atom's position
         inside the residue, not their absolute position in the Universe.
-
         """
         try:
             mapping = kwargs.pop('mapping')
         except KeyError:
             raise ValueError("CGUniverse requires the mapping keyword")
         # Atomistic Universe
-        # TODO: wrap this in try/except and process errors in constructing
-        # atomistic universe properly.
-        self.atu = mda.Universe(*args, **kwargs)
+        atu = self.atu = mda.Universe(*args, **kwargs)
 
         # Coarse grained Universe
         # Make a blank Universe for myself.
         super(CGUniverse, self).__init__()
 
-        # Fake up some beads
-        # TODO: Names, better topology etc
-        beads = self._apply_map(mapping)
-#        beads = [AtomGroup(self.atu.atoms[m]) for m in mapping]
+        # Set up CG Topology
+        nbeads = 0
+        nbead_res = 0
+        # Bead topology
+        bead_resnames = []
+        bead_atomresindex = []
+        bead_masses = []
+        bead_atomnames = []
+        # for traj mapping
+        atom_to_bead = np.zeros(len(atu.atoms), dtype=int)
+        weights = np.zeros(len(atu.atoms), dtype=float)
+        # Loop over residues in atomistic system and determine beads
+        for residue in atu.residues:
+            resmap = mapping[residue.resname]
+            bead_resnames.append(residue.resname)
 
-        beadgroup = [Bead(b, i, 'BEAD', 'BEAD',
-                          b[0].resname, b[0].resid, b[0].segid,
-                          1.0, 1.0) for i, b in enumerate(beads)]
-        for b in beadgroup:
-            b.universe = self
-        self.atoms = AtomGroup(beadgroup)
-        self.beads = beadgroup
+            for i, bead in enumerate(resmap):
+                atom_to_bead[residue.atoms.ix[bead]] = nbeads
+                nbeads += 1  # increment after as we want 0-based
+                # mass weighting of positions
+                masses = residue.atoms.masses[bead]
+                weights[residue.atoms.ix[bead]] = masses / masses.sum()
+                # bead attributes
+                bead_masses.append(masses.sum())
+                bead_atomnames.append('{}_{}'.format(residue.resname, i+1))
+                bead_atomresindex.append(nbead_res)
+            nbead_res += 1  # post increment again
 
-        # TODO: Wrap this in try except to catch errors in making fake Reader
+        self._topology = topology.Topology(nbeads, nbead_res, 1,
+                                           atom_resindex=bead_atomresindex,
+                                           attrs=[
+                                               ta.Masses(bead_masses),
+                                               ta.Atomnames(bead_atomnames),
+                                               ta.Resnames(bead_resnames),
+        ])
+        self.filename = atu.filename
+        self._generate_from_topology()
+
         # This replaces load_new in a traditional Universe
-        self.trajectory = CGTraj(self.atu.trajectory, self.beads)
-
-    def _apply_map(self, mapping):
-        """Apply the mapping scheme to the beads"""
-        beads = []
-        for r, m in mapping.items():
-            for res in self.atu.selectAtoms('resname {}'.format(r)).residues:
-                beads.extend([AtomGroup(res.atoms[idx]) for idx in m])
-
-        return beads
+        self.trajectory = CGTraj(self.atu.trajectory, atom_to_bead, weights)
 
     def __repr__(self):
-        return "<CG Universe with {} beads>".format(len(self.beads))
+        return "<CG Universe with {} beads>".format(len(self.atoms))
